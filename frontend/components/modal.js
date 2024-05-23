e914d3e5f0524e6479a34d21dac112fd76eed91f
Antonio Mongelli <68474838+L0NGW0LF@users.noreply.github.com>
2024-05-23 19:48:37 +0200

    Reliability fixes
    added role button to Patteners.js :26 and modal.js :13

--- frontend/components/modal.js
+++ frontend/components/modal.js
@@ -10,7 +10,7 @@ const Modal = ({isVisible, onClose}) => {
     }
 
     return(
-        <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center' id='wrapper' onClick={(handleClose)}>
+        <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center' id='wrapper' onClick={(handleClose)} role="button">
             <div className="w-[1600px] flex flex-col">
                 <button className="text-white text-xl place-self-end" onClick={() => onClose()}>X</button>
                 <div className="bg-white p-4 rounded text-black">
