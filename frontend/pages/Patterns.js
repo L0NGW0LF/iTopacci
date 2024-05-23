31d1473d132fd5269ebc89aaabd4e47b78c8a444
Antonio Mongelli <68474838+L0NGW0LF@users.noreply.github.com>
2024-05-23 19:54:10 +0200

    Refix
    removed roles

--- frontend/pages/Patterns.js
+++ frontend/pages/Patterns.js
@@ -23,7 +23,7 @@ return (
         <div class="md:flex-grow">
           <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">{item.attributes.Nome_Pattern}</h2>
           <p class="leading-relaxed">Descrizione: {item.attributes.Descrizione_pattern.Descrizione}</p>
-          <a class="text-indigo-500 inline-flex items-center mt-4" onClick={() => setShowModal(true)} role="button">Learn More
+          <a class="text-indigo-500 inline-flex items-center mt-4" onClick={() => setShowModal(true)}>Learn More
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
