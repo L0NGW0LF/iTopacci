02566504fd8fce7c581512c3d905b243485caaba
Antonio Mongelli <68474838+L0NGW0LF@users.noreply.github.com>
2024-06-08 19:34:46 +0200

    Update SignUp.js

--- frontend/pages/SignUp.js
+++ frontend/pages/SignUp.js
@@ -45,23 +45,23 @@ export default function SignUp() {
         <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
           <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
           <div className="relative mb-4">
-            <label for="username" className="leading-7 text-sm text-gray-600">Username</label>
+            <label  className="leading-7 text-sm text-gray-600">Username</label>
             <input type="username" id="username" name="username" placeholder="Enter Username" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
           <div className="relative mb-4">
-            <label for="name" className="leading-7 text-sm text-gray-600">Name</label>
+            <label  className="leading-7 text-sm text-gray-600">Name</label>
             <input type="name" id="name" name="name" placeholder="Enter Name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
           <div className="relative mb-4">
-            <label for="surname" className="leading-7 text-sm text-gray-600">Surname</label>
+            <label  className="leading-7 text-sm text-gray-600">Surname</label>
             <input type="surname" id="surname" name="surname" placeholder="Enter Surname" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
           <div className="relative mb-4">
-            <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
+            <label  className="leading-7 text-sm text-gray-600">Email</label>
             <input type="email" id="email" name="email" placeholder="Enter Email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
           <div className="relative mb-4">
-            <label for="password" className="leading-7 text-sm text-gray-600">Password</label>
+            <label  className="leading-7 text-sm text-gray-600">Password</label>
             <input type="passwd" id="password" name="password" placeholder="Enter Password" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
           <button type="submit" className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Confirm</button>
